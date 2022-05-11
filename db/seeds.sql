bb8fe7bfe4266f0603cfedc8c1d652768b967974
soulslurpee <charles.d.thompson87@gmail.com>
2022-05-10 20:26:02 -0400

    added db folder with seeds.sql inside

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE character_library
+
+INSERT INTO character 
+  (name, class, level, str, dex, con, int, wis, cha)
+VALUES
+  ('Frodo Baggins', 'Rogue', 4, 8, 16, 14, 10, 12, 10),
+  ('Legolas', 'Ranger', 9, 12, 18, 10, 12, 14, 12),
+  ('Gimli', 'Fighter', 7, 14, 8, 16, 12, 12, 8);
