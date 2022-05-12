2a2446731f99fb97da5149cf7050b1e1b86778b7
Zachary Thomas <sccr0123@gmail.com>
2022-05-11 23:35:08 -0400

    Created Character Model

---
+++ models/Character.js
@@ -0,0 +1,53 @@
+const { Model } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// create our User model
+class Character extends Model {}
+
+// create fields/columns for User model
+Character.init({
+	id: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	class: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	level: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	str: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	dex: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	con: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	int: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	wis: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	cha: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+});
+
+module.exports = Character;
