94ad9df169fbf09b1604722d98102773fcbf900f
Zachary Thomas <sccr0123@gmail.com>
2022-05-16 22:50:58 -0400

    Added User Login and Logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -58,4 +58,48 @@ router.delete("/:id", (req, res) => {
 		.catch((err) => res.status(500).json(err));
 });
 
+//Login
+router.post("/login", (req, res) => {
+	// expects {email: 'lernantino@gmail.com', password: 'password1234'}
+	User.findOne({
+		where: {
+			email: req.body.email,
+		},
+	}).then((dbUserData) => {
+		if (!dbUserData) {
+			res.status(400).json({
+				message: "No user with that email address!",
+			});
+			return;
+		}
+
+		const validPassword = dbUserData.checkPassword(req.body.password);
+		console.log(validPassword);
+
+		if (!validPassword) {
+			res.status(400).json({ message: "Incorrect password!" });
+			return;
+		}
+
+		req.session.save(() => {
+			req.session.user_id = dbUserData.id;
+			req.session.username = dbUserData.username;
+			req.session.loggedIn = true;
+
+			res.json({ user: dbUserData, message: "You are now logged in!" });
+		});
+	});
+});
+
+//Logout
+router.post("/logout", (req, res) => {
+	if (req.session.loggedIn) {
+		req.session.destroy(() => {
+			res.status(204).end();
+		});
+	} else {
+		res.status(404).end();
+	}
+});
+
 module.exports = router;
