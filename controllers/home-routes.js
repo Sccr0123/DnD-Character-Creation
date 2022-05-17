c7d5d41c6c6796cb690c94f59b7d15d720c1ae35
Zachary Thomas <sccr0123@gmail.com>
2022-05-16 23:18:58 -0400

    Added Login and Signup Routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -87,4 +87,22 @@ router.get("/Character/:id", (req, res) => {
 		});
 });
 
+router.get("/login", (req, res) => {
+	if (req.session.loggedIn) {
+		res.redirect("/");
+		return;
+	}
+
+	res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+	if (req.session.loggedIn) {
+		res.redirect("/");
+		return;
+	}
+
+	res.render("signup");
+});
+
 module.exports = router;
