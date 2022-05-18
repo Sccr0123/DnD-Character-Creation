ad3656a9beb53d8ef54c886de0544724f211b2eb
Zachary Thomas <sccr0123@gmail.com>
2022-05-17 20:12:12 -0400

    Edited Dashboard Route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -5,6 +5,9 @@ const { Character, User } = require("../models");
 router.get("/", (req, res) => {
 	console.log("======================");
 	Character.findAll({
+		where: {
+			user_id: req.session.user_id,
+		},
 		attributes: [
 			"id",
 			"name",
@@ -40,28 +43,28 @@ router.get("/", (req, res) => {
 		});
 });
 
-router.put('/edit/:id', (req, res) => {
-    Character.update(req.body, {
-        individualHooks: true,
-        where: {
-            id: req.params.id
-        }
-    })
-    .then((dbCharacterData) => {
-        if (dbCharacterData) {
-            res.status(404).json({ message: 'No character with this id' });
-            return;
-        }
-        res.json(dbCharacterData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+router.put("/edit/:id", (req, res) => {
+	Character.update(req.body, {
+		individualHooks: true,
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((dbCharacterData) => {
+			if (dbCharacterData) {
+				res.status(404).json({ message: "No character with this id" });
+				return;
+			}
+			res.json(dbCharacterData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
-router.get('/new_character', (req, res) => {
-    res.render('creation');
+router.get("/new_character", (req, res) => {
+	res.render("creation");
 });
 
 module.exports = router;
