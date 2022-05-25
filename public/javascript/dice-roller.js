f724792b3d83d4b7c2c584f2f48713aed936a2f8
Zachary Thomas <sccr0123@gmail.com>
2022-05-24 20:42:13 -0400

    Removed a console.log

--- public/javascript/dice-roller.js
+++ public/javascript/dice-roller.js
@@ -13,82 +13,80 @@ const wis = document.querySelector("#characterWIS").value.trim();
 const cha = document.querySelector("#characterCHA").value.trim();
 
 const return_mod = (score) => {
-  switch (score) {
-    case 0:
-      return 'DEAD';
-    case 1:
-      return '-5';
-    case 2:
-    case 3:
-      return '-4';
-    case 4:
-    case 5:
-      return '-3';
-    case 6:
-    case 7:
-      return '-2';
-    case 8:
-    case 9:
-      return '-1';
-    case 10:
-    case 11:
-      return '0';
-    case 12:
-    case 13:
-      return '+1';
-    case 14:
-    case 15:
-      return '+2';
-    case 16:
-    case 17:
-      return '+3';
-    case 18:
-    case 19:
-      return '+4';
-    case 20:
-    case 21:
-      return '+5';
-  }
-}
-
+	switch (score) {
+		case 0:
+			return "DEAD";
+		case 1:
+			return "-5";
+		case 2:
+		case 3:
+			return "-4";
+		case 4:
+		case 5:
+			return "-3";
+		case 6:
+		case 7:
+			return "-2";
+		case 8:
+		case 9:
+			return "-1";
+		case 10:
+		case 11:
+			return "0";
+		case 12:
+		case 13:
+			return "+1";
+		case 14:
+		case 15:
+			return "+2";
+		case 16:
+		case 17:
+			return "+3";
+		case 18:
+		case 19:
+			return "+4";
+		case 20:
+		case 21:
+			return "+5";
+	}
+};
 
 async function rollBtnHandler(event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  var roll = Math.floor(Math.random() * (18 - 3) + 3);
+	var roll = Math.floor(Math.random() * (18 - 3) + 3);
 
-  switch (event.target) {
-    case rollSTRBtn:
-      document.getElementById("characterSTR").value = roll;
-      console.log(return_mod(roll))
-      document.getElementById("strMod").innerText = return_mod(roll);
-      break
-    case rollDEXBtn:
-      document.getElementById("characterDEX").value = roll;
-      document.getElementById("dexMod").innerText = return_mod(roll);
-      break
-    case rollCONBtn:
-      document.getElementById("characterCON").value = roll;
-      document.getElementById("conMod").innerText = return_mod(roll);
-      break
-    case rollINTBtn:
-      document.getElementById("characterINT").value = roll;
-      document.getElementById("intMod").innerText = return_mod(roll);
-      break
-    case rollWISBtn:
-      document.getElementById("characterWIS").value = roll;
-      document.getElementById("wisMod").innerText = return_mod(roll);
-      break
-    case rollCHABtn:
-      document.getElementById("characterCHA").value = roll;
-      document.getElementById("chaMod").innerText = return_mod(roll);
-      break
-  }
+	switch (event.target) {
+		case rollSTRBtn:
+			document.getElementById("characterSTR").value = roll;
+			document.getElementById("strMod").innerText = return_mod(roll);
+			break;
+		case rollDEXBtn:
+			document.getElementById("characterDEX").value = roll;
+			document.getElementById("dexMod").innerText = return_mod(roll);
+			break;
+		case rollCONBtn:
+			document.getElementById("characterCON").value = roll;
+			document.getElementById("conMod").innerText = return_mod(roll);
+			break;
+		case rollINTBtn:
+			document.getElementById("characterINT").value = roll;
+			document.getElementById("intMod").innerText = return_mod(roll);
+			break;
+		case rollWISBtn:
+			document.getElementById("characterWIS").value = roll;
+			document.getElementById("wisMod").innerText = return_mod(roll);
+			break;
+		case rollCHABtn:
+			document.getElementById("characterCHA").value = roll;
+			document.getElementById("chaMod").innerText = return_mod(roll);
+			break;
+	}
 }
 
-rollSTRBtn.addEventListener('click', rollBtnHandler);
-rollDEXBtn.addEventListener('click', rollBtnHandler);
-rollCONBtn.addEventListener('click', rollBtnHandler);
-rollINTBtn.addEventListener('click', rollBtnHandler);
-rollWISBtn.addEventListener('click', rollBtnHandler);
-rollCHABtn.addEventListener('click', rollBtnHandler);
+rollSTRBtn.addEventListener("click", rollBtnHandler);
+rollDEXBtn.addEventListener("click", rollBtnHandler);
+rollCONBtn.addEventListener("click", rollBtnHandler);
+rollINTBtn.addEventListener("click", rollBtnHandler);
+rollWISBtn.addEventListener("click", rollBtnHandler);
+rollCHABtn.addEventListener("click", rollBtnHandler);
